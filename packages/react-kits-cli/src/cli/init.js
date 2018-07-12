f7421b85c58b9cd2c2851713894a8c58d4b97a52
Antony Budianto <antonybudianto@gmail.com>
2018-07-12 07:47:36 +0700

    change init copy logic

--- packages/react-kits-cli/src/cli/init.js
+++ packages/react-kits-cli/src/cli/init.js
@@ -1,4 +1,6 @@
-const template = require('fs-template');
+const copy = require('recursive-copy');
+const through = require('through2');
+const path = require('path');
 const install = require('yarn-install');
 
 const { log } = require('../util/log');
@@ -7,28 +9,35 @@ const { resolveDir, resolveCwd } = require('../util/path');
 function init(name) {
   log(`Creating "${name}" app...`);
 
-  const vars = {
-    name
-  };
   const inDir = resolveDir('../../template');
   const outDir = resolveCwd('./' + name);
-  const options = {
-    source: inDir,
-    target: outDir,
-    vars
-  };
 
-  template(options, (err, createdFiles) => {
-    if (err) throw err;
-    log('Project generated!');
-
-    log('Installing dependencies...');
-    install({
-      cwd: resolveCwd(name)
+  copy(inDir, outDir, {
+    overwrite: true,
+    dot: true,
+    transform: function(src, dest, stats) {
+      if (['.md', '.json'].indexOf(path.extname(src)) === -1) {
+        return null;
+      }
+      return through(function(chunk, enc, done) {
+        var str = chunk.toString();
+        var output = str.replace('{{name}}', name);
+        done(null, output);
+      });
+    }
+  })
+    .then(() => {
+      log('Project generated successfully.');
+      log('Installing dependencies...');
+      install({
+        cwd: resolveCwd(name)
+      });
+      log('Dependencies installed!');
+      log(`Run "cd ${name} && npm start" to get started!`);
+    })
+    .catch(err => {
+      log('Error while generating project', err);
     });
-    log('Dependencies installed!');
-    log(`Run "cd ${name} && npm start" to get started!`);
-  });
 }
 
 module.exports = init;
