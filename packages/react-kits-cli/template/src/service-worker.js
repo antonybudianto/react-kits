e47dea895dfe38b5a79cfad7f40d0d9fa39004cf
Antony Budianto <antonybudianto@gmail.com>
2018-08-29 13:58:43 +0700

    add sw cache

--- packages/react-kits-cli/template/src/service-worker.js
+++ packages/react-kits-cli/template/src/service-worker.js
@@ -1,3 +1,22 @@
 /* eslint-disable no-undef */
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest)
+
+var CACHE_NAME = 'my-pwa-cache-v1'
+var urlsToCache = ['/']
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request)
+    })
+  )
+})
