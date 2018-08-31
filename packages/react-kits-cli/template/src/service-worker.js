f5871cba53700e1a7c77f73d6bbb665e50128533
Antony Budianto <antonybudianto@gmail.com>
2018-08-31 13:37:00 +0700

    remove consolelog

--- packages/react-kits-cli/template/src/service-worker.js
+++ packages/react-kits-cli/template/src/service-worker.js
@@ -13,7 +13,6 @@ self.addEventListener('install', function(event) {
 })
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url)
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request)
