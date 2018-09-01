f142620ed3c2f998623737d8a61a8b11c1a6fd14
Antony Budianto <antonybudianto@gmail.com>
2018-09-01 14:51:37 +0700

    home route use homepath

--- packages/react-kits-cli/template/src/routes/Home/index.js
+++ packages/react-kits-cli/template/src/routes/Home/index.js
@@ -1,7 +1,8 @@
 import HomeView from './HomeView'
+import { HOME_PATH } from '../../constant/url'
 
 export default {
-  path: '/',
+  path: HOME_PATH,
   component: HomeView,
   exact: true,
   auth: true
