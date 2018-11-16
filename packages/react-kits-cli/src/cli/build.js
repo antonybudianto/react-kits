dcaf72cf4d6322ce5c8b9ff734e30a7c85aa5559
Antony Budianto <antonybudianto@gmail.com>
2018-11-16 07:47:11 +0700

    fix build exit code

--- packages/react-kits-cli/src/cli/build.js
+++ packages/react-kits-cli/src/cli/build.js
@@ -8,50 +8,57 @@ const { dir_dist } = require('../config/project.config');
 
 const paths = [dir_dist];
 
-function cleanBuild(done) {
-  del(paths).then(() => {
+function cleanBuild() {
+  return del(paths).then(() => {
     log('Build clean done.');
-    done();
   });
 }
 
-function build(label, config, cb = () => {}) {
+function build(label, config) {
   log(`Building production bundle for "${label}"...`);
   const ins = webpack(config);
-  ins.run((err, stats) => {
-    if (err) {
-      console.error(err.stack || err);
-      if (err.details) {
-        console.error(err.details);
+
+  const promise = new Promise((resolve, reject) => {
+    ins.run((err, stats) => {
+      if (err) {
+        console.error(err.stack || err);
+        if (err.details) {
+          console.error(err.details);
+        }
+        reject(err);
       }
-      throw err;
-    }
 
-    const info = stats.toJson();
+      const info = stats.toJson();
 
-    if (stats.hasErrors()) {
-      console.error(info.errors);
-      throw info.errors;
-    }
+      if (stats.hasErrors()) {
+        log('>> sss');
+        console.error(info.errors);
+        reject(info.errors);
+      }
 
-    if (stats.hasWarnings()) {
-      console.warn(info.warnings);
-    }
-    console.log(stats.toString(config.stats));
-    log(`Done building "${label}"!`);
-    cb();
-  });
-}
-
-try {
-  cleanBuild(() => {
-    log('NODE_ENV: ' + process.env.NODE_ENV);
-    log('APP_ENV: ' + process.env.APP_ENV);
-    build('client', clientConfig, () => {
-      build('server', serverConfig);
+      if (stats.hasWarnings()) {
+        console.warn(info.warnings);
+      }
+      console.log(stats.toString(config.stats));
+      log(`Done building "${label}"!`);
+      resolve();
     });
   });
-} catch (e) {
-  console.error(e);
-  process.exit(1);
+
+  return promise;
 }
+
+log('NODE_ENV=' + process.env.NODE_ENV);
+log('APP_ENV=' + process.env.APP_ENV);
+
+cleanBuild()
+  .then(() => {
+    return build('client', clientConfig);
+  })
+  .then(() => {
+    return build('server', serverConfig);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
