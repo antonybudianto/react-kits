133bec698629d6c4677ee1bab68570349802a86f
Antony Budianto <antonybudianto@gmail.com>
2018-12-08 09:50:37 +0700

    integrate react-ua

--- packages/react-kits-cli/template/src/reducers/index.js
+++ packages/react-kits-cli/template/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
-import { userAgentReducer } from './user-agent'
 import { userReducer } from './user'
 
 export default combineReducers({
-  user: userReducer,
-  userAgent: userAgentReducer
+  user: userReducer
 })
--- packages/react-kits-cli/template/src/routes/index.js
+++ packages/react-kits-cli/template/src/routes/index.js
@@ -1,17 +1,13 @@
 import { renderRoutes, matchRoutes } from 'react-router-config'
-import { UAParser } from 'ua-parser-js'
 
 import HomeRoute from './Home'
 import NotFoundRoute from './NotFoundPage'
 import AboutRoute from './About'
-import { initUserAgent } from '../reducers/user-agent'
 
 const routes = [{ ...HomeRoute }, { ...AboutRoute }, { ...NotFoundRoute }]
 
 export const getInitialData = (req, store) => {
   const path = req.path
-  const ua = new UAParser(req.headers['user-agent']).getResult()
-  store.dispatch(initUserAgent(ua))
   return matchRoutes(routes, path).map(({ route }) => {
     const promises = []
     if (route.loadData) {
